a01f80b6c6f4aa8bd1fb6f21be12d5ca8c0aee6d
ethonimus <48042245+ethoart@users.noreply.github.com>
2026-07-20 22:56:55 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
   },
   server: {
     host: true,
-    allowedHosts: ['oms.hyperoms.xyz', 'localhost', '127.0.0.1']
+    allowedHosts: ['oms.hyperoms.xyz', 'erp.hyperoms.xyz', 'localhost', '127.0.0.1']
   }
 });
